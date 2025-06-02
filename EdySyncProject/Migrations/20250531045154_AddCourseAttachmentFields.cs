19be581c2511f10f9ac3f18fbd696de5a3550b75
Lakshya Mishra <lakshyamishra05@gmail.com>
2025-06-02 14:36:02 +0530

    Initial Commit

---
+++ EdySyncProject/Migrations/20250531045154_AddCourseAttachmentFields.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EdySyncProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCourseAttachmentFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeLimitMinutes",
+                table: "Assessments");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AttachmentName",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AttachmentUrl",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttachmentName",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "AttachmentUrl",
+                table: "Courses");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TimeLimitMinutes",
+                table: "Assessments",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ EdySyncProject/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿public class Course
+{
+    public Guid CourseId { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string MediaUrl { get; set; }
+
+    public Guid InstructorId { get; set; }
+    public User Instructor { get; set; }
+    public string AttachmentUrl { get; set; }
+    public string AttachmentName { get; set; }
+
+    public ICollection<Assessment> Assessments { get; set; }
+    public ICollection<Enrollment> Enrollments { get; set; }
+}
